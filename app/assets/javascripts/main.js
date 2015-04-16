d8574834c8c0cc01a4435efe28d6f79d4f885080
benshrimpton <benjaminshrimpton@yahoo.co.uk>
2015-04-16 19:13:32 -0400

    Resolved db migration issue

---
+++ app/assets/javascripts/main.js
@@ -0,0 +1,8 @@
+$(document).on('ready page:load', function () {
+
+
+	console.log("dom ready")
+
+
+});
+
--- app/models/project.rb
+++ app/models/project.rb
@@ -1,5 +1,4 @@
 class Project < ActiveRecord::Base
 	validates :title, :presence => true, :length => { :minimum => 3}, :exclusion => { :in => ["Title", "title"] }
 	validates :description, :length => { :minimum => 10 }
-	has_many :comments
 end
